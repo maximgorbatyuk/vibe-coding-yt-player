1d584550d7522102932dfa3a521d6b70efb55b4c
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2026-01-09 21:18:09 +0500

    Initial Commit

---
+++ YoutubeLivePlayer/YoutubeLivePlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeLivePlayerApp.swift
+//  YoutubeLivePlayer
+//
+//  Created by Maxim Gorbatyuk on 09.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeLivePlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
